9beefda812a092d7bd7fa97c4689c7fc30322776
William Daniel Blackman <blackmanbilly@gmail.com>
2020-04-27 10:44:24 -0500

    Authorization component added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import KandyKorner from './components/KandyKorner';
+import Auth from "./components/auth/Auth"
 
 
-ReactDOM.render(<KandyKorner />, document.getElementById("root"))
+
+    ReactDOM.render(localStorage.getItem("kandy__customer") ? <KandyKorner /> : <Auth />, document.getElementById("root"))
+
